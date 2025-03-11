7de7160225b5a06942cd11ddb4ba76591c36f9f8
Kianahazhir <Kiana.hazhir.97@gmail.com>
2025-03-11 23:05:47 +0330

    Simple-ToDo-List-Python.py

--- Simple-ToDo-List-Python.py
+++ Simple-ToDo-List-Python.py
@@ -1,23 +1,36 @@
 import tkinter as tk
+import json
 from tkinter import messagebox
+def save_tasks(tasks):
+    with open("tasks.json", "w") as file:
+        json.dump(tasks, file)
 
 def add_task():
     task = entry.get()
     if task:
         listbox.insert(tk.END, task)
         entry.delete(0, tk.END)
+        
     else:
         messagebox.showwarning("Warning", "Please enter a task.")
-
+        save_tasks(tasks)
+def load_tasks():
+    try:
+        with open("tasks.json", "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
 def remove_task():
     selected_task_index = listbox.curselection()
     if selected_task_index:
         listbox.delete(selected_task_index)
     else:
         messagebox.showwarning("Warning", "Please select a task to remove.")
-
+save_tasks(tasks)
 def clear_list():
     listbox.delete(0, tk.END)
+    save_tasks(tasks)
+tasks = load_tasks()
 
 # Create the main application window
 root = tk.Tk()
